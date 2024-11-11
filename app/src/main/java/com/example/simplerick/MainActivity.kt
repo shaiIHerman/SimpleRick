9cf362c483b4b798cdddc1e0561e03df4bd4b8a7
Shai Herman <shai.herman@IL-MacBP-ShaiM3.local>
2024-11-11 14:49:55 +0200

    basic compose and api interface

--- app/src/main/java/com/example/simplerick/MainActivity.kt
+++ app/src/main/java/com/example/simplerick/MainActivity.kt
@@ -3,23 +3,16 @@ package com.example.simplerick
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.network.KtorClient
-import com.example.network.models.domain.Character
+import com.example.simplerick.screens.CharacterDetailsScreen
 import com.example.simplerick.ui.theme.SimpleRickTheme
-import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     private val ktorClient = KtorClient()
@@ -27,24 +20,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
         setContent {
-            var character by remember {
-                mutableStateOf<Character?>(null)
-            }
-
-            // val scope = rememberCoroutineScope() not needed because block gives you the coroutine scope
-
-            LaunchedEffect(key1 = Unit, block = {
-                delay(2000)
-                character = ktorClient.getCharacter(1)
-            })
             SimpleRickTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Column {
-                        Greeting("Android")
-                        Text(text = character?.name ?: "No character")
-                    }
+                    CharacterDetailsScreen(ktorClient = ktorClient, characterId = 19)
                 }
             }
         }
--- network/src/main/java/com/example/network/KtorClient.kt
+++ network/src/main/java/com/example/network/KtorClient.kt
@@ -30,20 +30,32 @@ class KtorClient {
         }
     }
 
-    suspend fun getCharacter(id: Int): Character {
-        return client.get("character/$id").body<RemoteCharacter>().toDomainCharacter()
+    suspend fun getCharacter(id: Int): ApiOperation<Character> {
+        return safeApiCall {
+            client.get("character/$id").body<RemoteCharacter>().toDomainCharacter()
+        }
+    }
+
+    private inline fun <T> safeApiCall(apiCall: () -> T): ApiOperation<T> {
+        return try {
+            ApiOperation.Success(data = apiCall())
+        } catch (e: Exception) {
+            ApiOperation.Failure(e)
+        }
     }
 }
 
-//@Serializable
-//data class Character(
-//    val id: Int,
-//    val name: String,
-//    val origin: Origin
-//
-//) {
-//    @Serializable
-//    data class Origin(
-//        val name: String
-//    )
-//}
+sealed interface ApiOperation<T> {
+    data class Success<T>(val data: T) : ApiOperation<T>
+    data class Failure<T>(val exception: Exception) : ApiOperation<T>
+
+    fun onSuccess(block: (T) -> Unit): ApiOperation<T> {
+        if (this is Success) block(data)
+        return this
+    }
+
+    fun onFailure(block: (Exception) -> Unit): ApiOperation<T> {
+        if (this is Failure) block(exception)
+        return this
+    }
+}
